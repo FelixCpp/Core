3fc328b89cc8f0e614bb9119884df27f6c6589a1
FelixCPP <fix.busch@gmail.com>
2021-01-05 13:14:44 +0100

    Wrote some comments about the functions and attributes of an AudioTarget

--- Core/Include/Core/Audio/AudioTarget.hpp
+++ Core/Include/Core/Audio/AudioTarget.hpp
@@ -17,22 +17,35 @@ namespace Core
 	class AudioTarget {
 	public:
 
+		/* the default constructor */
 		AudioTarget();
+
+		/* a virtual destructor */
 		virtual ~AudioTarget();
 
+		/* creates a buffer and returns the SoundSource from it */
 		SoundSource loadSound(const std::string & filepath);
 
 	private:
 
+		/* initializes the audio */
 		bool initAudio();
+
+		/* destroys the audio */
 		void destroyAudio();
+
+		/* destroys every buffer and SoundSources */
 		void destroyBuffers();
 
 	private:
 
+		/* an OpenAL device */
 		ALCdevice_struct * device;
+
+		/* an OpenAL context */
 		ALCcontext_struct * context;
 
+		/* a map from filepath to SoundBuffer. Kind of a cache */
 		std::unordered_map<std::string, SoundBuffer> buffers;
 
 	};
