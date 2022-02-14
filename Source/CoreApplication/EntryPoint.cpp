75568f4d4bc6894521121e0818535dc8e4f529c3
FelixCPP <fix.busch@gmail.com>
2022-02-15 00:30:21 +0100

    Initial commit

---
+++ Source/CoreApplication/EntryPoint.cpp
@@ -0,0 +1,10 @@
+#include <CoreApplication/EntryPoint.hpp>
+
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+	std::cout << "Hello from Core" << std::endl;
+	std::cin.get();
+	return 0;
+}
